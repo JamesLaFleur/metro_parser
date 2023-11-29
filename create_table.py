d139b1f215d8acb8c9f970f3552228d6437cd43b
Ilya Isaev <ilyajoice@gmail.com>
2023-11-29 23:36:35 +0300

    progect created

---
+++ create_table.py
@@ -0,0 +1,42 @@
+import xlsxwriter
+import os
+from main import parseData, URL
+
+NAMES = ['Артикул', 'Название', 'Новая цена', 'Старая цена', 'Бренд', 'Ссылка']
+
+def writer(parser):  
+    path = os.path.join(os.getcwd(), 'data.xlsx')
+    book = xlsxwriter.Workbook(path)
+    page = book.add_worksheet("Product")
+    bold = book.add_format({'bold': True})
+
+    row = 1
+    column = 0
+
+    page.set_column("A:A", 20)
+    page.set_column("B:B", 20)
+    page.set_column("C:C", 50)
+    page.set_column("D:D", 50)
+    page.set_column("E:E", 50)
+    page.set_column("F:F", 50)
+
+    title_column = 0
+    for name in NAMES:
+        page.write(0, title_column, name, bold)
+        title_column += 1
+
+
+    for items in parser(URL):
+        for item in items:
+            page.write(row, column, item.get('ProductID'))
+            page.write(row, column+1, item.get('Product_name'))
+            page.write(row, column+2, item.get('Actual_price'))
+            page.write(row, column+3, item.get('Old_price'))
+            page.write(row, column+4, item.get('Brand'))
+            page.write(row, column+5, item.get('Product_link'))
+            row += 1
+
+    book.close()
+
+
+writer(parseData)
---
+++ main.py
@@ -0,0 +1,83 @@
+import math
+from bs4 import BeautifulSoup
+import requests
+
+URL = 'https://online.metro-cc.ru/category/bakaleya/krupy-bobovye'
+PRODUCT_URL = 'https://online.metro-cc.ru'
+
+PRODUCT_CARD_SELECTOR = '.product-card:not(.is-out-of-stock)'
+
+BRAND_SELECTOR = '.product-attributes__list li:last-child'
+ARTICLE_SELECTOR = '.product-page-content__article'
+NAME_SELECTOR = 'product-card-name'
+LINK_SELECTOR = 'product-card-photo__link'
+
+PRICE_WRAPPER_SELECTOR = 'product-unit-prices__old-wrapper'
+PRICE_WRAPPER_NEW_SELECTOR = 'product-unit-prices__actual-wrapper'
+PRICE_SELECTOR = 'product-price__sum-rubles'
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Mobile Safari/537.36'
+}
+
+def parseData(url: str) -> list[dict[str, str]]:
+    print('Parse data started')
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'lxml')
+
+    products_count_text = soup.select('.heading-products-count')[0]
+    products_count = int(products_count_text.text.split()[0])
+    pages_count = math.ceil(products_count / 30) 
+
+    for page in range(1, pages_count+1):
+        print('START PARSE FOR PAGE: ', page)
+        if(page == 1):
+            yield parseProductsInfo(soup)
+        else:
+            yield parseProductsInfo(getPageData(url, page))
+
+
+def getPageData(url: str, page: int) -> BeautifulSoup:
+    html_text = requests.get(url + '?page=' + str(page)).text
+    return BeautifulSoup(html_text, 'lxml')
+
+
+def parseProductsInfo(soupEx: BeautifulSoup) -> list[dict[str, str]]:
+    products_in_stock = soupEx.select(PRODUCT_CARD_SELECTOR)
+
+    result = []
+
+    for product in products_in_stock:
+        product_name = product.find('a',  class_ = NAME_SELECTOR).text.strip()
+        product_act_price = product.find('div', class_ = PRICE_WRAPPER_NEW_SELECTOR).find('span', class_ = PRICE_SELECTOR).text
+        product_old_price = product.find('div', class_ = PRICE_WRAPPER_SELECTOR).find('span', class_ = PRICE_SELECTOR)
+        
+        product_link = PRODUCT_URL + product.find('a', class_ = LINK_SELECTOR).get('href')
+        
+
+        prod_info = requests.get(product_link).text
+        soup = BeautifulSoup(prod_info, 'lxml')
+
+        product_id_container = soup.select(ARTICLE_SELECTOR)
+        product_id = product_id_container[0].text.split()[1] if product_id_container else '--'
+        product_info_items = soup.select(BRAND_SELECTOR)
+
+        for item in product_info_items:
+            if "Бренд" in item.text:
+                link = item.find('a')
+                if link: 
+                    brand_name = item.find('a').text.strip()
+
+
+        data = {
+            'ProductID': product_id,
+            'Product_name': product_name,
+            'Actual_price':product_act_price,
+            'Old_price': product_old_price.text if product_old_price else '--',
+            'Product_link': product_link,
+            'Brand': brand_name,
+        }
+
+        result.append(data)
+
+    return result
